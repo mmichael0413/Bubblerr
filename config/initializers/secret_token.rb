a8dcdc5c631bc802b2778bde10ccad0c225b71e1
Mike Michaelevich <mmichael0413@gmail.com>
2011-07-01 11:25:58 -0400

    add ideas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubblerr::Application.config.secret_token = '55508fde7b332c0c2341927df6d6b17005208fe789a50932b9f5f97a24f388128cce240c5e522eb5ab6683bb4fa6e8b9832b60594821c3bf52cfb68c82b54c63'
